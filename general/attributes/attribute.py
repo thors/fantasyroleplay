1f32ce365ca9d510476a7957a8c96404952067ce
Thorsten Möllers <github@thorzten.de>
2013-03-07 17:04:15 +0100

    Update...

---
+++ general/attributes/attribute.py
@@ -0,0 +1,9 @@
+class Attribute:
+    "Generic description of what an attibute is. Each attribute gets a minimum and maximum value within the typical range for humans. By adding a species to the Character, the minimum and maximum value might be modified.
+    name="base"
+    min=0
+    max=100
+
+    __init__(self, character):
+        print("Instantiating attibute...")
+    
